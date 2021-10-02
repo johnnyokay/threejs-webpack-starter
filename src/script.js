beaad4e196a84b3053b9d18c58c044739f02ec66
johnnyaokay <49286386+johnnyaokay@users.noreply.github.com>
2021-10-02 14:37:27 +0100

    initial setup

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+import map from '../earth.jpg'
+
 // Debug
 const gui = new dat.GUI()
 
@@ -13,15 +15,18 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const geometry = new THREE.SphereBufferGeometry(1, 30, 30);
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(map)
+})
+
+//material.color = new THREE.Color(0xff0000)
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
+const sphere = new THREE.Mesh(geometry, material)
 scene.add(sphere)
 
 // Lights
@@ -40,8 +45,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -66,8 +70,8 @@ camera.position.z = 2
 scene.add(camera)
 
 // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 
 /**
  * Renderer
@@ -84,16 +88,63 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+function calcPosFromLatLonRad(lat, lon) {
+    var phi = (90 - lat) * (Math.PI / 180)
+    var theta = (lon + 180) * (Math.PI / 180)
+
+    let x = -(Math.sin(phi) * Math.cos(theta))
+    let z = (Math.sin(phi) * Math.sin(theta))
+    let y = (Math.cos(phi))
+
+    return { x, y, z }
+}
+
+function convertLatLngToCartesian(p) {
+    let lat = (90 - p.lat) * Math.PI / 180
+    let lng = (180 + p.lng) * Math.PI / 180
+
+    let x = -Math.sin(lat) * Math.cos(lng)
+    let y = Math.sin(lat) * Math.sin(lng)
+    let z = Math.cos(lat)
+
+    return { x, y, z }
+}
+
+//red markers
+let mesh = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.01, 10, 10),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+
+let point1 = {
+    lat: 50.4501,
+    lng: 30.5234
+}
+
+let mexico = {
+    lat: 32.1656,
+    lng: -82.9001
+}
+
+//let pos = convertLatLngToCartesian(point1)
+let pos = calcPosFromLatLonRad(mexico.lat, mexico.lng)
+
+console.log(pos.x)
+mesh.position.set(pos.x, pos.y, pos.z)
+
+//mesh.position.set(1, 0, 0)
+
+scene.add(mesh)
+
+const tick = () => {
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    //sphere.rotation.y = .5 * elapsedTime
 
     // Update Orbital Controls
-    // controls.update()
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
